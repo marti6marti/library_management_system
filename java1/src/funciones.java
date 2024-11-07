aaa46ed6a097332968b0af3453c468fd4841fbe8
serhii <y7913092_w@iespoblenou.org>
2024-11-07 14:21:22 +0100

    java 1

---
+++ java1/src/funciones.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class funciones {
+    public static void test(){
+        for (int i = 0; i < 10; i++) {
+            System.out.println("HOla");
+        }
+    }
+}
